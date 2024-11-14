91447645bd4bdbee221357f8a6514e71b08e267a
choi14123 <choichanghwan@choechanghwan-ui-MacBookPro.local>
2024-11-14 22:30:01 +0900

    style: 코드 스타일 변경

--- src/main/java/TheNaeunEconomy/user/service/UserBatchService.java
+++ src/main/java/TheNaeunEconomy/user/service/UserBatchService.java
@@ -3,7 +3,6 @@ package TheNaeunEconomy.user.service;
 import TheNaeunEconomy.user.Repository.UserRepository;
 import TheNaeunEconomy.user.domain.User;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import java.time.LocalDateTime;
@@ -21,7 +20,7 @@ public class UserBatchService {
         List<User> usersToDelete = userRepository.findByIsDeletedTrueAndDeleteDateBefore(threeMonthsAgo);
 
         for (User user : usersToDelete) {
-            userRepository.delete(user); // 실제 데이터베이스에서 삭제
+            userRepository.delete(user); // 3개월 후 실제 데이터베이스에서 삭제
             System.out.println("User " + user.getEmail() + " has been deleted.");
         }
     }
