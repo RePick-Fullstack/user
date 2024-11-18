df595086db72156ae045bc0c26298e86251774c5
choi14123 <choichanghwan@choechanghwan-ui-MacBookPro.local>
2024-11-19 01:25:27 +0900

    refactor: 카카오 이름 불러오게 리펙토링

--- src/main/java/TheNaeunEconomy/user/service/UserService.java
+++ src/main/java/TheNaeunEconomy/user/service/UserService.java
@@ -15,7 +15,7 @@ import org.springframework.http.ResponseEntity.BodyBuilder;
 public interface UserService {
     Optional<User> findUserByEmail(String email);
 
-    User registerUser(String email);
+    User registerUser(String email, String name);
 
     HttpStatus saveUser(AddUserRequest request);
 
