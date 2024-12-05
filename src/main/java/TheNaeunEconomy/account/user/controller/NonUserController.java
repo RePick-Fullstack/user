c790494b90c2fcec119daaeb17e562dc45db203a
choi14123 <choichanghwan@choechanghwan-ui-MacBookPro.local>
2024-12-05 20:58:38 +0900

    refactor: 엔드 포인트 수정

--- src/main/java/TheNaeunEconomy/account/user/controller/NonUserController.java
+++ src/main/java/TheNaeunEconomy/account/user/controller/NonUserController.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/api/v1")
+@RequestMapping("/api/v1/users")
 public class NonUserController {
 
     private final NonUserServiceImpl nonUserService;
