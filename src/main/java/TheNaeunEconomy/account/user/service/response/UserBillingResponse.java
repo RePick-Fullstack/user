0be3d09de761203a4031e36ffb91caeb3c681133
Choi Chang Hwan <63833865+choi14123@users.noreply.github.com>
2024-12-18 00:06:56 +0900

    feat: billing 조회

---
+++ src/main/java/TheNaeunEconomy/account/user/service/response/UserBillingResponse.java
@@ -0,0 +1,10 @@
+package TheNaeunEconomy.account.user.service.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class UserBillingResponse {
+    private boolean isBilling;
+}
